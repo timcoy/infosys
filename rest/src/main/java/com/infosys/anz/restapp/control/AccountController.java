13ef1ec3e2b100f2443d97e76e561c61a1449ad9
Tim Coy <tim.coy@me.com>
2021-02-09 08:56:26 +1100

    remove unused imports

--- rest/src/main/java/com/infosys/anz/restapp/control/AccountController.java
+++ rest/src/main/java/com/infosys/anz/restapp/control/AccountController.java
@@ -1,10 +1,7 @@
 package com.infosys.anz.restapp.control;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
